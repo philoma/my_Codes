26f06985af0c1fa308c03101fb4135fc41afe0b3
Mohd Faisal <faisal02v@gmail.com>
2022-08-23 23:01:48 +0530

    Update dp_fibonacci_series.cpp

--- dp_fibonacci_series.cpp
+++ dp_fibonacci_series.cpp
@@ -25,3 +25,40 @@ int main()
     }
     cout<<f(n,dp);
 }
+
+
+// //using bottom-up/tabulation method
+// class Solution {
+// public:
+//     int fib(int n) {
+//         int dp[n+1];
+//         dp[0]=0;
+//         if(n>=1)
+//             {dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }}
+//         return dp[n];        
+//     }
+// };
+
+
+
+
+//just variable ka kheil
+class Solution {
+public:
+    int fib(int n) {
+        int prev1=0;
+        int prev2=1;
+        if(n==0||n==1)
+            return n;
+        int cur;
+        for(int i=2;i<=n;i++){
+            cur=prev1+prev2;
+            prev1=prev2;
+            prev2=cur;
+        }
+        return cur;
+    }
+};
