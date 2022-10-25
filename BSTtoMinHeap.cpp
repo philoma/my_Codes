777213d4247697ba65b7f0f740b5453258532c95
Mohd Faisal <faisal02v@gmail.com>
2022-10-25 23:02:49 +0530

    Update BSTtoMinHeap.cpp

--- BSTtoMinHeap.cpp
+++ BSTtoMinHeap.cpp
@@ -4,7 +4,7 @@ void inorder(BinaryTreeNode* root,vector<int> &ans)
     if(root==NULL)
     {
         return;
-    }
+    } 
     inorder(root->left,ans);
     ans.push_back(root->data);
     inorder(root->right,ans);
