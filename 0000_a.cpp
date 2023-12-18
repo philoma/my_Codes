0ca0493239c2725733b9b4e83b535e2c49ae513f
Mohd Faisal <faisal02v@gmail.com>
2023-12-19 01:38:54 +0530

    Update 0000_a.cpp

--- 0000_a.cpp
+++ 0000_a.cpp
@@ -13,3 +13,17 @@ bool checkArmstrong(int n){
 	return sum==n;
 
 }
+
+
+
+int sumOfAllDivisors(int n){
+	// Write your code here.
+	  int sum=0;
+    for(int i=1;i<=n;i++){
+
+        sum+=(n/i)*i;
+
+    }
+
+    return sum; 
+}
